de0d19bf50f31e93b0c037b12d7f5a40021ee06f
JanakB <bhudathokijanak@gmail.com>
2023-12-19 18:31:38 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# mali-flower-e-com-" 
---
+++ about.php
@@ -0,0 +1,190 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php @include 'header.php'; ?>
+
+<section class="heading">
+    <h3>about us</h3>
+    <p> <a href="home.php">home</a> / about </p>
+</section>
+
+<section class="about">
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-1.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3>why choose us?</h3>
+            <p>Customers can have flowers delivered to their own address or send them as gifts to friends, family, or colleagues for various occasions like birthdays, anniversaries, or special events</p>
+            <a href="shop.php" class="btn">shop now</a>
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="content">
+            <h3>what we provide?</h3>
+            <p> provides a convenient way for customers to shop for flowers
+                 without the need to visit a physical store. With just a few clicks, customers can browse through the available options,
+                 compare prices, and place orders at any time of the day. </p>
+            <a href="contact.php" class="btn">contact us</a>
+        </div>
+
+        <div class="image">
+            <img src="images/about-img-2.jpg" alt="">
+        </div>
+
+    </div>
+
+    <div class="flex">
+
+        <div class="image">
+            <img src="images/about-img-3.jpg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>who we are?</h3>
+            <p>WE are the best flower provider in valley with a shortest delivery charges and times, we provides fresh flower.</p>
+            <a href="#reviews" class="btn">clients reviews</a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="reviews" id="reviews">
+
+    <h1 class="title">client's reviews</h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/pic-1.png" alt="">
+            <p>The website was easy to navigate, and the selection of flowers was impressive. The bouquet arrived on time, beautifully arranged, and the roses were fresh and fragrant. .</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Hari</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-2.png" alt="">
+            <p>. The customer service was excellent, as they promptly responded to my inquiries and ensured that the bouquet was delivered on our special day..</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Dr. Sandhuk</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-3.png" alt="">
+            <p>The quality of the flowers was outstanding, and I will definitely use this flower e-commerce service again.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Poshan</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-4.png" alt="">
+            <p>I was impressed by the assortment of romantic arrangements available. The website made it easy to choose the perfect bouquet, and I was even able to add a small gift to accompany the flowers.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Aasha</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-5.png" alt="">
+            <p>The delivery was prompt, and the bouquet was breathtaking. It definitely set the mood for a memorable celebration. This flower e-commerce platform provided a seamless and delightful experience</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Himal</h3>
+        </div>
+
+        <div class="box">
+            <img src="images/pic-6.png" alt="">
+            <p>. I'm thrilled with my purchase and will continue using this flower e-commerce service for all my floral needs.</p>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <h3>Binita</h3>
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<?php @include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
